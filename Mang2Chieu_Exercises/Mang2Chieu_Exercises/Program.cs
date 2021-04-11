87529e252d15a19fb6c77c27f0f3f00d77a36280
unknown <daniel.luan.tran@gmail.com>
2021-04-11 14:43:07 +0700

    Lap trinh theo tu duy to chuc ham Them vao xu ly mang so nguyen va so thuc Lam BT 315: Tim gia tri lon nhat cua mang so thuc

---
+++ Mang2Chieu_Exercises/Mang2Chieu_Exercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mang2Chieu_Exercises
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Tinh tong cac phan tu tren ma tran
+            int[,] A;
+            A = XL_MaTran.Nhapmatran_Songuyen();
+            //int S = XL_MaTran.TinhTong(A);
+
+            //Xuat ma tran so nguyen
+            XL_MaTran.Xuatmatran_Songuyen(A);
+
+            //Nhap ma tran so thuc
+            double[,] B;
+            B = XL_MaTran.Nhapmatran_Sothuc();
+
+            //Xuat ma tran so thuc
+            XL_MaTran.Xuatmatran_Sothuc(B);
+        }
+    }
+}
---
+++ Mang2Chieu_Exercises/Mang2Chieu_Exercises/XL_MaTran.cs
@@ -0,0 +1,121 @@
+using System;
+
+namespace Mang2Chieu_Exercises
+{
+    class XL_MaTran
+    {
+        public static int[,] Nhapmatran_Songuyen()
+        {
+            //Nhap ma tra so nguyen A
+            Console.Write("Nhap so dong cua ma tran so nguyen A: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so cot cua ma tran so nguyen A: ");
+            int M = int.Parse(Console.ReadLine());
+
+            int[, ] A = new int[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.WriteLine($"Nhap A[{i},{j}]");
+                    A[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return A;
+        }
+        public static void Xuatmatran_Songuyen(int[,] A)
+        {
+            Console.WriteLine();
+            //Xuat ma tran so nguyen A
+            Console.WriteLine($"Xuat ma tran");
+
+            for (int j = 0; j < A.GetLength(1); j++)
+            {
+                Console.Write($"{A[0, j]} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                Console.Write($"{A[i, 0]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static int TinhTong(int[,] A)
+        {
+            //Tinh tong cac phan tu tren ma tran so nguyen
+            Console.WriteLine($"Tinh tong cac phan tu");
+            int S = 0;
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    S = S + A[i, j];
+                }
+            }
+            Console.WriteLine($"Tong la: {S}");
+            return S;
+        }
+
+        public static double[,] Nhapmatran_Sothuc()
+        {
+            //Nhap ma tra so thuc
+            Console.Write("Nhap so dong cua ma tran so thuc B: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap so cot cua ma tran so thuc B: ");
+            int M = int.Parse(Console.ReadLine());
+
+            double[,] A = new double[N, M];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.WriteLine($"Nhap B[{i},{j}]");
+                    A[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+            return A;
+        }
+        public static void Xuatmatran_Sothuc(double[,] B)
+        {
+            //Xuat ma tran so thuc
+            Console.WriteLine($"Xuat ma tran so thuc B");
+            for (int j = 0; j < B.GetLength(1); j++)
+            {
+                Console.Write($"{B[0, j]} ");
+            }
+            Console.WriteLine();
+            
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                Console.Write($"{B[i, 0]} ");
+            }
+            Console.WriteLine();
+
+            //Console.WriteLine(B[1, 1]);
+        }
+
+        public static double TimMax(double[,] B)
+        {
+            //Tim max cua ma tran so thuc
+            Console.WriteLine("Tim max cua ma tran so thuc");
+            double Max = 0;
+            for (int i = 0; i < B.GetLength(0); i++)
+            {
+                for (int j = 0; j < B.GetLength(1); j++)
+                {
+                    if (B[i,j] > Max)
+                    {
+                        Max = B[i, j];
+                    }
+                }
+            }
+            return Max;
+        }
+    }
+}
