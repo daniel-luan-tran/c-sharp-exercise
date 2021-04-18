ab23bfc167f5643fc910a714485840c04c849d89
unknown <daniel.luan.tran@gmail.com>
2021-04-18 12:29:01 +0700

    Update Bai 334: Dem so luong so thuc duong trong ma tran

---
+++ Mang2Chieu_Exercises/Bai_334.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Mang2Chieu_Exercises
+{
+	class Bai_334
+	{
+		public static void PositiveCount(double[,]arr)
+		{
+			int count = 0;
+			int i, j;
+			for (i = 0; i < arr.GetLength(0); i++)
+            {
+				for (j = 0; j < arr.GetLength(1); j++)
+                {
+					if (arr[i, j] > 0)
+                    {
+						count++;
+                    }
+                }
+            }
+			if (count == 0)
+			{
+				Console.WriteLine("Khong co so duong nao trong mang so thuc");
+			}
+			else
+            {
+				Console.WriteLine($"Co {count} so duong trong mang so thuc");
+			}
+		}
+	}
+}
--- Mang2Chieu_Exercises/Program.cs
+++ Mang2Chieu_Exercises/Program.cs
@@ -16,6 +16,9 @@ namespace Mang2Chieu_Exercises
             //Xuat ma tran so nguyen
             XL_MaTran.Xuatmatran_Songuyen(A);
 
+            //Bai 316: Kiem tra so nguyen chan be hon 2015
+            Bai_316.KTSonguyenChanBehon2015(A);
+
             //Nhap ma tran so thuc
             double[,] B;
             B = XL_MaTran.Nhapmatran_Sothuc();
@@ -26,14 +29,14 @@ namespace Mang2Chieu_Exercises
             //Bai 315: Tim Max cua ma tran so thuc B
             Bai_315.TimMax(B);
 
-            //Bai 316: Kiem tra so nguyen chan be hon 2015
-            Bai_316.KTSonguyenChanBehon2015(A);
-
             //Bai 318: Tong gia tri am cua ma tran so thuc
             Bai_318.TongSothucAm(B);
 
             //Bai 319: Sap xep so thuc tang dan tu tren xuong va tu trai sang
             Bai_319.SapxepSothucTangdanTrenxuongTraisang(B);
+
+            //Bai 334: Dem so luong so thuc duong trong ma tran
+            Bai_334.PositiveCount(B);
         }
     }
 }
