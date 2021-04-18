845791b4a8adf79898fc4f02154ff6dea7baa83c
Daniel Luan Tran <daniel.luan.tran@gmail.com>
2021-04-18 13:30:45 +0700

    Update Bai 335: Dem so luong so nguyen to trong ma tran so nguyen

--- Mang2Chieu_Exercises/Bai_316.cs
+++ Mang2Chieu_Exercises/Bai_316.cs
@@ -3,7 +3,7 @@ namespace Mang2Chieu_Exercises
 {
     class Bai_316
     {
-        public static bool KTSonguyenChanBehon2015(int[,] A)
+        public static void KTSonguyenChanBehon2015(int[,] A)
         {
             int i, j;
             bool KT = false;
@@ -17,8 +17,25 @@ namespace Mang2Chieu_Exercises
                     }
                 }
             }
-            Console.WriteLine(KT);
-            return KT;
+            if (KT == true)
+            {
+                Console.WriteLine("Cac so nguyen to chan va be hon 2015: ");
+                for (i = 0; i < A.GetLength(0); i++)
+                {
+                    for (j = 0; j < A.GetLength(1); j++)
+                    {
+                        if (A[i, j] % 2 == 0 && A[i, j] < 2015)
+                        {
+                            Console.Write($"{A[i, j]}  ");
+                        }
+                    }
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Khong co so nguyen to nao chan va be hon 2015");
+            }
         }
     }
 }
---
+++ Mang2Chieu_Exercises/Bai_335.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Mang2Chieu_Exercises
+{
+	class Bai_335 //Dem so luong so nguyen to trong ma tran so nguyen
+	{
+		public static void PrimeCount(int[,] arr)
+		{
+			int count = 0;
+			int i, j;
+			int k = 2;
+			for (i = 0; i < arr.GetLength(0); i++)
+			{
+				for (j = 0; j < arr.GetLength(1); j++)
+				{
+					if (arr[i, j] == 2)
+					{
+						count++;
+					}
+					while (k < arr[i, j])
+                    {
+						if (arr[i, j] % k == 0)
+                        {
+							k++;
+                        }
+						else
+                        {
+							count++;
+						}
+					}
+				}
+			}
+			if (count == 0)
+            {
+				Console.WriteLine("Khong co so nguyen to nao");
+            }
+			else
+            {
+				Console.WriteLine($"Co {count} so nguyen to trong ma tran so nguyen!");
+            }
+		}
+	}
+}
